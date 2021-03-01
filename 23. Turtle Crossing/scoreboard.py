2045efc6c65cdf81dba528a89645be0f0167cc17
EX1ST3NCE <prasadnitin05@gmail.com>
2021-03-01 10:34:45 +0530

    Initial Commit

---
+++ 23. Turtle Crossing/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("courier", 12, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.level = 1
+        self.penup()
+        self.goto(-550, 350)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over!", align="center", font=FONT)
