42aaa8e648d01b6a172528288255f324a15736c8
EX1ST3NCE <prasadnitin05@gmail.com>
2021-03-09 09:14:53 +0530

    Added Exception Handling

--- 26. NATO Alphabet/main.py
+++ 26. NATO Alphabet/main.py
@@ -3,8 +3,16 @@ import pandas as pd
 file = pd.read_csv("nato_phonetic_alphabet.csv")
 name_dict = {row.letter: row.code for (index, row) in file.iterrows()}
 
-name_data = input("Enter your name : ").upper()
-name_list = [name for name in name_data]
 
-output_list = [name_dict[letter] for letter in name_list]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter your name : ").upper()
+    try:
+        output_list = [name_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
